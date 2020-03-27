eeebfc5590fbc224681d81fb3e756f2143d5058d
kodeerkarthik <kkrishnegowdaostb1@gmail.com>
2020-03-27 13:35:10 +0530

    devise completed

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+config.gem "declarative_authorization", :source => "http://gemcutter.org"
 # Initialize the Rails application.
 Blog::Application.initialize!
